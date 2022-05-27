19ff026975d884bc37cb8d969ac19b21818001b3
qqq000qqq@aliyun.com <xqq1258460261>
2022-05-27 17:14:49 +0800

    20220227

---
+++ src/components/FormulaEditor/index.jsx
@@ -0,0 +1,6 @@
+
+function FormualEditor(){
+
+}
+
+export default FormualEditor
---
+++ src/views/index/index.jsx
@@ -0,0 +1,9 @@
+import FormualEditor from 'components/FormualEditor'
+
+function Index(){
+    return (
+        <FormualEditor className="FormualEditor"></FormualEditor>
+    )
+}
+
+export default Index
