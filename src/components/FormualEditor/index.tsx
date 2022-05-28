2d02cde116b642bdddfc7d22b073d5db14a8d184
xjh <qqq000qqq@aliyun.com>
2022-05-28 23:22:29 +0800

    20220528

--- src/components/FormualEditor/index.tsx
+++ src/components/FormualEditor/index.tsx
@@ -1,29 +1,45 @@
-import './index.scss'
-import {TokenType} from 'util/formualPaser'
+import {useRef,useState} from 'react'
+import "./index.scss"
+import {TokenType,FormualToken,parseFormual} from "util/formualPaser"
+import {
+    Button
+} from "@mui/material";
 
 function FormualEditor(props) {
 
-    const {formualTokens} = props;
+    const editorCodeBox = useRef(null)
+
+    let [formual,setFormual] = useState<string>(props.formual);
+
+    const [formualTokens,setFormualTokens] = useState<Array<FormualToken>>(parseFormual(formual))
     
+    const editorCodeBoxInput = event => {
+        const _formual = editorCodeBox.current.innerText;
+        setFormual(_formual)
+        setFormualTokens(parseFormual(_formual))
+    }
+
+    const copyFormual = () => {
+        console.log(formual)
+        console.log(formualTokens)
+    }
+
     return (
-        <div className="formual-editor" suppressContentEditableWarning contentEditable={true}>
-            {
-                formualTokens.map(token => {
-                    return (
-                        token.type === TokenType.FIELD
-                        ?
-                        <span 
-                            className={token.type} key={token.value} 
-                            contentEditable={false}
-                            dangerouslySetInnerHTML={{__html: `&ZeroWidthSpace;${token.value}&ZeroWidthSpace;`}}>
-                        </span>
-                        :
-                        <span className={token.type} key={token.value}>
-                            {token.value}
-                        </span>
-                    )
-                })
-            }
+        <div className="formual-editor">
+            <div className="editor-head">      
+                <Button variant="outlined" size="small" onClick={copyFormual}>复制</Button>
+            </div>
+            <div ref={editorCodeBox} className="editor-code-box" suppressContentEditableWarning contentEditable={true} onInput={editorCodeBoxInput}>
+                {
+                    formualTokens.map(token => {
+                        return (
+                            <span className={token.type} key={token.value} suppressContentEditableWarning contentEditable={token.type !== TokenType.FIELD}>
+                                {token.value}
+                            </span>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- src/views/index/index.tsx
+++ src/views/index/index.tsx
@@ -1,40 +1,13 @@
 import './index.scss'
 import FormualEditor from 'components/FormualEditor'
-import {FormualToken,parseFormual} from 'util/formualPaser'
 
 function Index() {
 
-    const formualTokens: Array<FormualToken> = parseFormual("SUM(金额)*998.88")
-    // const formualTokens: Array<FormualToken> = [
-    //     {
-    //         type: "function",
-    //         value: "SUM"
-    //     },
-    //     {
-    //         type: "bracket",
-    //         value: "("
-    //     },
-    //     {
-    //         type: "field",
-    //         value: "字段"
-    //     },
-    //     {
-    //         type: "bracket",
-    //         value: ")"
-    //     },
-    //     {
-    //         type: "operator",
-    //         value: "+"
-    //     },
-    //     {
-    //         type: "number",
-    //         value: "999.99"
-    //     }
-    // ]
+    const formual = "SUM(金额)*998.88"
 
     return (
         <div>
-            <FormualEditor formualTokens={formualTokens}></FormualEditor>
+            <FormualEditor formual={formual}></FormualEditor>
         </div>
     )
 
